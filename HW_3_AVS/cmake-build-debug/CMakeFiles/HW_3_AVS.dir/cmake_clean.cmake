9c0d1815381e6714ebaa9632309e5af070b42656
aokalugin <aokalugin@edu.hse.ru>
2020-11-19 02:02:04 +0300

    HW3 Done

---
+++ HW_3_AVS/cmake-build-debug/CMakeFiles/HW_3_AVS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW_3_AVS.dir/main.cpp.o"
+  "HW_3_AVS"
+  "HW_3_AVS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW_3_AVS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
